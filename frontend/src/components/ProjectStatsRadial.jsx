0ce46bdd299debb15ffebbede553a8f5907fb72f
nripan <nripanudayan@gmail.com>
2026-04-05 15:41:46 +0530

    added analytics components

--- frontend/src/components/ProjectStatsRadial.jsx
+++ frontend/src/components/ProjectStatsRadial.jsx
@@ -16,8 +16,8 @@ export const ProjectStatsRadial = ({ projects = [] }) => {
   ];
 
   return (
-    <div className="w-full outline-none focus:outline-none focus:ring-0 lg:h-94 bg-[#0C1A2B] rounded-xl p-4">
-      <h2 className="text-[#B6FF3B] outline-none focus:outline-none focus:ring-0 font-semibold mb-2">Project Overview</h2>
+    <div className="w-full relative outline-none focus:outline-none focus:ring-0 lg:h-94 h-70 flex lg:flex-col bg-[#0C1A2B] rounded-xl p-4">
+      <h2 className="text-[#B6FF3B]  outline-none focus:outline-none focus:ring-0 font-semibold mb-2">Project Overview</h2>
 
       <ResponsiveContainer className='outline-0'>
         <RadialBarChart
@@ -28,11 +28,11 @@ export const ProjectStatsRadial = ({ projects = [] }) => {
           data={data}
           className='outline-0'
         >
-          <RadialBar dataKey="value" className='outline-none focus:outline-none focus:ring-0' cornerRadius={10} />
+          <RadialBar dataKey="value" className='outline-none focus:outline-none focus:ring-0' cornerRadius={90} />
         </RadialBarChart>
       </ResponsiveContainer>
 
-      <div className="flex bg-[#0C1A2B] p-4 rounded-md outline-0 justify-around mt-2 text-xs">
+      <div className="flex absolute bottom-0 right-0 gap-1 bg-[#0C1A2B] p-4 rounded-md outline-0 justify-center items-center mt-2 text-xs">
         <span className="text-green-400">Completed {completed}</span>
         <span className="text-blue-400">Active {active}</span>
         <span className="text-red-400">Pending {overdue}</span>
